7572aa765641a6c0ba15915d4ee7df4f555be368
apalski <annette.drapalski@gmail.com>
2017-12-28 09:32:27 +1100

    Setup database cleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -18,6 +18,17 @@ RSpec.configure do |config|
   config.infer_base_class_for_anonymous_controllers = false
   config.infer_spec_type_from_file_location!
   config.use_transactional_fixtures = false
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
 end
 
 ActiveRecord::Migration.maintain_test_schema!
