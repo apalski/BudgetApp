9aa0e2c7d361153a7abe28506e703c922482351f
Annette Drapalski <annette.drapalski@gmail.com>
2018-01-05 09:07:38 +1100

    Update users_controller.rb
    Fixed indentation errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,58 +1,58 @@
 class UsersController < ApplicationController
 
   def index
-		@users = User.all.sort_by {|user| user.name.downcase}
-	end
+    @users = User.all.sort_by {|user| user.name.downcase}
+  end
 
-	def new
-		if !current_user
-			@user = User.new
-		else
-			redirect_to user_path(current_user), notice: "You are already signed in"
-		end
-	end
+  def new
+    if !current_user
+      @user = User.new
+    else
+      redirect_to user_path(current_user), notice: "You are already signed in"
+    end
+  end
 
-	def create
-		@user = User.new(user_params)
-		if @user.save
-			session[:user_id] = @user.id
-			redirect_to root_path, notice: "You have successfully joined Budget App"
-		else
-		 	render :new
-		end
-	end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "You have successfully joined Budget App"
+    else
+      render :new
+    end
+  end
 
-	def show
-		set_user
-	end
+  def show
+    set_user
+  end
 
-	def edit
-		set_user
-	end
+  def edit
+    set_user
+  end
 
-	def update
-		set_user
-		if @user == current_user && @user.update(user_params)
-			redirect_to user_path(@user)
-		else
-			render :edit
-		end
-	end
+  def update
+    set_user
+    if @user == current_user && @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
 
-	def destroy
-		set_user.destroy
-		respond_to do |format|
-			format.html {redirect_to users_path, notice: "User was successfully deleted"}
-		end
-	end
+  def destroy
+    set_user.destroy
+    respond_to do |format|
+      format.html {redirect_to users_path, notice: "User was successfully deleted"}
+    end
+  end
 
-	private
+private
 
-	def set_user
-		@user = User.find(params[:id])
-	end
+  def set_user
+    @user = User.find(params[:id])
+  end
 
-	def user_params
-		params.require(:user).permit(:name, :email, :uid, :password)
-	end
+  def user_params
+    params.require(:user).permit(:name, :email, :uid, :password)
+  end
 end
