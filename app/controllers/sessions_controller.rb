cf72e5efd7e2f2acb647803e7c2c0cfd80a10774
Annette Drapalski <annette.drapalski@gmail.com>
2018-01-05 09:08:25 +1100

    Update sessions_controller.rb
    Fixed indentation errors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,8 +14,8 @@ class SessionsController < ApplicationController
   def destroy
     session.clear
     respond_to do |format|
-	format.html { redirect_to new_session_path,
-        notice: "You are signed out" }
+      format.html { redirect_to new_session_path,
+                    notice: "You are signed out" }
     end
   end
 end
