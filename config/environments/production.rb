26710056b12492e50a5a6b59d6b41ae801e3a313
apalski <annette.drapalski@gmail.com>
2018-01-12 14:07:20 +1100

    Fix rubocop violations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,12 +5,13 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
   config.read_encrypted_secrets = true
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
   config.assets.js_compressor = :uglifier
   config.assets.compile = false
-  config.action_controller.asset_host = ENV.fetch("ASSET_HOST", ENV.fetch("APPLICATION_HOST"))
+  config.action_controller.asset_host = ENV.fetch("ASSET_HOST",
+                                                  ENV.fetch("APPLICATION_HOST"))
   config.log_level = :debug
-  config.log_tags = [ :request_id ]
+  config.log_tags = [:request_id]
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = SMTP_SETTINGS
@@ -23,17 +24,19 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
   config.active_record.dump_schema_after_migration = false
-  
+
   if ENV.fetch("HEROKU_APP_NAME", "").include?("staging-pr-")
     ENV["APPLICATION_HOST"] = ENV["HEROKU_APP_NAME"] + ".herokuapp.com"
   end
   config.middleware.use Rack::CanonicalHost, ENV.fetch("APPLICATION_HOST")
   config.middleware.use Rack::Deflater
   config.public_file_server.headers = {
-    "Cache-Control" => "public, max-age=31557600",
+    "Cache-Control" => "public, max-age=31557600"
   }
-  config.action_mailer.default_url_options = { host: ENV.fetch("APPLICATION_HOST") }
-  config.action_mailer.asset_host = { host: ENV.fetch("ASSET_HOST", ENV.fetch("APPLICATION_HOST")) }
+  config.action_mailer.default_url_options =
+    { host: ENV.fetch("APPLICATION_HOST") }
+  config.action_mailer.asset_host = { host: ENV.fetch("ASSET_HOST",
+                                                      ENV.fetch("APPLICATION_HOST")) }
   config.force_ssl = true
 end
 Rack::Timeout.timeout = (ENV["RACK_TIMEOUT"] || 10).to_i
