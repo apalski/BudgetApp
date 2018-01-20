6e2d4acdde42794c21135e988c6acc5db29e607b
apalski <annette.drapalski@gmail.com>
2018-01-20 17:09:24 +1100

    Migrate expenses table

---
+++ db/migrate/20180120042303_create_expenses.rb
@@ -0,0 +1,10 @@
+class CreateExpenses < ActiveRecord::Migration[5.1]
+  def change
+    create_table :expenses do |t|
+      t.string :name, null: false
+      t.date :due_date
+      t.string :frequency, null: false
+      t.decimal :bill_estimate
+    end
+  end
+end
