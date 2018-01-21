91990fc39c80bb8aa80e0aaa51b02df530a97f65
apalski <annette.drapalski@gmail.com>
2018-01-21 15:24:15 +1100

    Create User factory, refactor User specs to use factory and shoulda matchers

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,56 +1,31 @@
 require "rails_helper"
 
 describe User do
-  context "when all attributes are correct" do
-    it "is created" do
-      user = User.create(name: "John Doe",
-                         email: "user@gmail.com")
+  context "validations" do
+    it do
+      build(:user, email: "user@gmail.com")
 
-      expect(user).to be_a User
+      should validate_presence_of(:name)
     end
-  end
 
-  context "when name not given" do
-    it "raises an error" do
-      user = User.create(email: "user@gmail.com")
+    it do
+      build(:user, name: "John Doe")
 
-      expect { user.save! }
-        .to raise_error("Validation failed: Name must be entered")
+      should validate_presence_of(:email)
     end
-  end
 
-  context "when email not given" do
-    it "raises an error" do
-      user = User.create(name: "John Doe")
+    it do
+      create(:user, name: "John Doe", email: "user@gmail.com")
+      build(:user, name: "John Doe", email: "user2@gmail.com")
 
-      expect { user.save! }
-        .to raise_error("Validation failed: Email must be entered")
+      should validate_uniqueness_of(:name)
     end
-  end
 
-  context "when name is not unique" do
-    it "raises an error" do
-      user = User.create(name: "John Doe",
-                         email: "user@gmail.com")
-      user.save!
-      user2 = User.create(name: "John Doe",
-                          email: "user2@gmail.com")
+    it do
+      create(:user, name: "John Doe", email: "user@gmail.com")
+      build(:user, name: "Mary Doe", email: "user@gmail.com")
 
-      expect { user2.save! }
-        .to raise_error("Validation failed: Name already taken")
-    end
-  end
-
-  context "when email is not unique" do
-    it "raises an error" do
-      user = User.create(name: "John Doe",
-                         email: "user@gmail.com")
-      user.save!
-      user2 = User.create(name: "Mary Doe",
-                          email: "user@gmail.com")
-
-      expect { user2.save! }
-        .to raise_error("Validation failed: Email already taken")
+      should validate_uniqueness_of(:email)
     end
   end
 end
