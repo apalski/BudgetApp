5cab8628cad38ec0f443b82c1ea37d04eae71d9d
apalski <annette.drapalski@gmail.com>
2018-01-21 18:35:22 +1100

    Add Income

---
+++ db/migrate/20180120061737_create_incomes.rb
@@ -0,0 +1,11 @@
+class CreateIncomes < ActiveRecord::Migration[5.1]
+  def change
+    create_table :incomes do |t|
+      t.string :name, null: false
+      t.date :due_date
+      t.string :frequency, null: false
+      t.decimal :amount
+      t.timestamps
+    end
+  end
+end
