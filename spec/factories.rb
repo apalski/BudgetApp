561c4f7fe83dbd14a8405c38ec4b906455758b66
apalski <annette.drapalski@gmail.com>
2018-01-23 21:13:02 +1100

    Refactor User model spec and factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,6 @@
 FactoryBot.define do
   factory :user do
+    sequence(:name) { |n| "name#{n}" }
+    sequence(:email) { |n| "email#{n}" }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,30 +2,11 @@ require "rails_helper"
 
 describe User do
   context "validations" do
-    it do
-      build(:user, email: "user@gmail.com")
+    subject { build(:user) }
 
-      should validate_presence_of(:name)
-    end
-
-    it do
-      build(:user, name: "John Doe")
-
-      should validate_presence_of(:email)
-    end
-
-    it do
-      create(:user, name: "John Doe", email: "user@gmail.com")
-      build(:user, name: "John Doe", email: "user2@gmail.com")
-
-      should validate_uniqueness_of(:name)
-    end
-
-    it do
-      create(:user, name: "John Doe", email: "user@gmail.com")
-      build(:user, name: "Mary Doe", email: "user@gmail.com")
-
-      should validate_uniqueness_of(:email)
-    end
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_uniqueness_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:email) }
   end
 end
