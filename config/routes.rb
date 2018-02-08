d3a4ac43044c70169b7473b60f0b31619778fec4
apalski <annette.drapalski@gmail.com>
2018-02-08 20:52:19 +1100

    Add navbar to make sign out link available

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show, :edit, :update, :destroy]
   resources :budgets, only: [:new]
   root to: "home#show"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
   resources :sessions, only: [:destroy]
-
   scope "/:locale", locale: /en/ do
     resources :users, only: [:new]
     resources :budgets, only: [:new]
