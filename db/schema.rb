ffdb0b0d02ffb342faca42e2058830cd95c08530
Elle Meredith <ellemeredith@gmail.com>
2018-02-20 15:42:47 +1100

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,17 @@
-ActiveRecord::Schema.define(version: 20180211015149) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20180210040920) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,8 +32,10 @@ ActiveRecord::Schema.define(version: 20180211015149) do
   create_table "budgets", force: :cascade do |t|
     t.string "name", null: false
     t.string "budget_type", default: "basic"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_budgets_on_user_id"
   end
 
   create_table "expenses", force: :cascade do |t|
@@ -30,16 +45,6 @@ ActiveRecord::Schema.define(version: 20180211015149) do
     t.decimal "bill_estimate"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "goals", force: :cascade do |t|
-    t.string "name", null: false
-    t.date "due_date", null: false
-    t.decimal "amount", null: false
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_goals_on_user_id"
   end
 
   create_table "incomes", force: :cascade do |t|
@@ -60,4 +65,5 @@ ActiveRecord::Schema.define(version: 20180211015149) do
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
   end
+
 end
