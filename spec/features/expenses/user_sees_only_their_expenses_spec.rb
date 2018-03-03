70c0b8d72e36f0258f82b0059c2bdfe26eb5146c
apalski <annette.drapalski@gmail.com>
2018-03-04 09:21:25 +1100

    Add user can only see own expenses feature spec

---
+++ spec/features/expenses/user_sees_only_their_expenses_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+feature "user sees only their expenses" do
+  scenario "successfully" do
+    user = create(:user)
+    budget = create(:budget, user: user)
+    user_expense = create(:expense, budget: budget)
+    other_expense = create(:expense)
+    log_in_as user
+
+    visit expenses_path
+
+    expect(page).to have_text(I18n.t("expenses.index.title"))
+    expect(page).to have_text(user_expense.name)
+    expect(page).not_to have_text(other_expense.name)
+  end
+end
