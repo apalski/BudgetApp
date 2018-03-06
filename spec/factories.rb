2611e634ce12810e4a915dd34ea4fb2fc64fc771
apalski <annette.drapalski@gmail.com>
2018-03-06 20:44:42 +1100

    Refactor factories, remove unnecessary fields

--- spec/factories.rb
+++ spec/factories.rb
@@ -7,13 +7,13 @@ FactoryBot.define do
 
   factory :expense do
     sequence(:name) { |n| "name#{n}" }
-    frequency "Weekly"
+    frequency :weekly
     budget
   end
 
   factory :income do
     sequence(:name) { |n| "name#{n}" }
-    frequency :Fortnightly
+    frequency :fortnightly
   end
 
   factory :user do
