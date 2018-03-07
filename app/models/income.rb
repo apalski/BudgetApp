c3d128f179b6cf883d86f8d3aceadd646ca5df1a
apalski <annette.drapalski@gmail.com>
2018-03-07 14:49:13 +1100

    Refactor frequency enum

--- app/models/income.rb
+++ app/models/income.rb
@@ -1,7 +1,9 @@
 class Income < ActiveRecord::Base
-  belongs_to :budget
+  belongs_to :budget, dependent: :destroy
 
-  enum frequency: [:Weekly, :Fortnightly, :Monthly, :Quarterly, :Annually]
+  enum frequency: {
+    weekly: 0, fortnightly: 1, monthly: 2, quarterly: 3, annually: 4,
+  }   
 
   validates :name, :frequency, presence: true
   validates :name, uniqueness: true
