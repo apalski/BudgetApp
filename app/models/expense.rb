ae5217433af4d0547e054594dc95aa0c0247baef
apalski <annette.drapalski@gmail.com>
2018-03-13 10:41:55 +1100

    Add styling for views

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,11 +2,7 @@ class Expense < ActiveRecord::Base
   belongs_to :budget, dependent: :destroy
 
   enum frequency: {
-    weekly: 0,
-    fortnightly: 1,
-    monthly: 2,
-    quarterly: 3,
-    annually: 4,
+    weekly: 0, fortnightly: 1, monthly: 2, quarterly: 3, annually: 4,
   }
 
   validates :bill_estimate, :due_date, :frequency, :name, presence: true
