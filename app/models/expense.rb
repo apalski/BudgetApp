e437836059d2c3ff9fc233ddee30eb4d828c5d10
apalski <annette.drapalski@gmail.com>
2018-03-13 10:41:40 +1100

    Add validations for bill_estimate and due_date

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,14 +2,10 @@ class Expense < ActiveRecord::Base
   belongs_to :budget, dependent: :destroy
 
   enum frequency: {
-    weekly: 0,
-    fortnightly: 1,
-    monthly: 2,
-    quarterly: 3,
-    annually: 4,
+    weekly: 0, fortnightly: 1, monthly: 2, quarterly: 3, annually: 4,
   }
 
-  validates :name, :frequency, presence: true
+  validates :bill_estimate, :due_date, :frequency, :name, presence: true
   validates :name, uniqueness: true
   validates :frequency, inclusion: { in: Expense.frequencies.keys }
 
