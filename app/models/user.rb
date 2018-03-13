27867ebae14934cf3a97ed394495616155a6079a
apalski <annette.drapalski@gmail.com>
2018-03-13 15:04:04 +1100

    Replace validate_password method with allow_nil: true

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,9 @@ class User < ActiveRecord::Base
 
   validates :name, :email, presence: true, uniqueness: true
   validates :email, confirmation: true
-  validates :password, length: { in: 6..64, if: :validate_password? }
+  validates :password, length: { in: 6..64, allow_nil: true }
 
   def self.by_admin
     order(admin: :desc, name: :asc)
-  end
-
-  private
-
-  def validate_password?
-    password.present?
   end
 end
