3938e9cf7e5a7b32942e727c556619e033fc1cd5
apalski <annette.drapalski@gmail.com>
2018-03-13 14:39:02 +1100

    Add variable name to spec

--- spec/features/budgets/user_sees_only_their_budget_spec.rb
+++ spec/features/budgets/user_sees_only_their_budget_spec.rb
@@ -9,7 +9,9 @@ feature "user sees only their budget" do
 
     visit budgets_path
 
-    expect(page).to have_text(I18n.t("budgets.show.title"))
+    expect(page).to have_text(
+      I18n.t("budgets.show.title", name: user_budget.name),
+    )
     expect(page).to have_text(user_budget.name)
     expect(page).not_to have_text(other_budget.name)
   end
