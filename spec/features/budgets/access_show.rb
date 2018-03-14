85e64d24d00d17f978ccbdf5530ff1d97750a979
apalski <annette.drapalski@gmail.com>
2018-03-14 16:11:15 +1100

    Refactor budget controller to remove total_expenses logic from show view

--- spec/features/budgets/access_show.rb
+++ spec/features/budgets/access_show.rb
@@ -10,4 +10,16 @@ feature "views show page" do
 
     expect(page).to have_text(I18n.t("budgets.show.title", name: budget.name))
   end
+
+  scenario "correct expenses total shows" do
+    user = create(:user)
+    budget = create(:budget, user: user)
+    create(:expense, bill_estimate: 50, budget: budget)
+    create(:expense, bill_estimate: 150, budget: budget)
+    log_in_as user
+
+    visit budget_path(budget)
+
+    expect(page).to have_text("$250.00")
+  end
 end
